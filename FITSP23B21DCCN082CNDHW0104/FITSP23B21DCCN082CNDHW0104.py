3f84f30a332955bcf34df53ad3357a7ba28d7513
Chu Mai <chuthimai18245@gmail.com>
2023-05-14 10:01:13 +0700

    Bai 4
    add all_code function

--- FITSP23B21DCCN082CNDHW0104/FITSP23B21DCCN082CNDHW0104.py
+++ FITSP23B21DCCN082CNDHW0104/FITSP23B21DCCN082CNDHW0104.py
@@ -4,6 +4,8 @@
 # Lớp quản lý: D21CQCN10-B
 # Bài 04
 
+import numpy as np
+
 H = [[1, 0, 1, 1, 1, 0, 0],
      [0, 1, 0, 1, 1, 1, 0],
      [0, 0, 1, 0, 1, 1, 1]]
@@ -36,7 +38,7 @@ def take_column(n):
     return res
 
 
-def generation(arr):
+def next_combination(arr):
     last_arr = [i for i in range(columns-len(arr), columns)]
     if arr == last_arr:
         return [0 for i in range(0, len(arr))]
@@ -50,6 +52,7 @@ def generation(arr):
         return arr
 
 
+# Tìm dmin dựa theo số cột của ma trận kiểm tra H
 def find_dmin():
     s = 1
     res = [0]
@@ -60,7 +63,7 @@ def find_dmin():
 
     loop = True
     while loop:
-        res = generation(res)
+        res = next_combination(res)
         if is_zero(res):
             s += 1
             res = [i for i in range(0, s)]
@@ -71,7 +74,40 @@ def find_dmin():
             loop = False
 
 
-print(find_dmin())
+# Kiểm tra một vec-tơ cho trước có phải là một vec-tơ mã hợp lệ
+def check_correct_code(code):
+    H_T = np.array(H, dtype=int).transpose()
+    code = np.array(code, dtype=int).transpose()
+    code_dot_H_T = np.dot(code, H_T).tolist()
+    for i in range(0, len(code_dot_H_T)):
+        code_dot_H_T[i] %= 2
+    return is_zero(code_dot_H_T)
 
 
+def binary_generation(arr):
+    last_arr = [1 for i in arr]
+    if last_arr == arr:
+        return -1
+    else:
+        for i in range(len(arr)-1, -1, -1):
+            if arr[i] == 0:
+                arr[i] = 1
+                for j in range(i+1, len(arr)):
+                    arr[j] = 0
+                break
+        return arr
+
+
+# Liệt kê các từ mã cho bộ mã
+def all_codes():
+    codes = []
+    code = [0 for i in range(0, columns)]
+    while code != -1:
+        if check_correct_code(code):
+            codes.append(code)
+            print(code)
+            code = binary_generation(code)
+    return codes
+
+all_codes()
 
