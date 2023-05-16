7246053d194d47c40d9c68e50a910ad45ccc8ff7
Chu Mai <chuthimai18245@gmail.com>
2023-05-16 21:21:05 +0700

    Bai 01
    in ra cây quyết định

---  FITSP23B21DCCN082CNDHW0101/FITSP23B21DCCN082CNDHW0101.py
+++  FITSP23B21DCCN082CNDHW0101/FITSP23B21DCCN082CNDHW0101.py
@@ -9,9 +9,9 @@ import numpy as np
 from queue import Queue
 
 data = pd.read_csv("data.csv")
-# features = ["Temperature", "Heart Rate", "Pulse", "BPSYS", "BPDIA", "Oxygen Saturation", "PH", "Type"]
-standard = {"Temperature": [98, 100], "Heart Rate": [60, 99], "Pulse": [60, 99], "BPSYS": [90, 120], "BPDIA": [60, 80], "Oxygen Saturation": [0.95, 1], "PH": [7.3, 7.5]}
-features = data.keys()
+features = ["Temperature", "Heart Rate", "Pulse", "BPSYS", "BPDIA", "Oxygen Saturation", "PH", "Type"]
+standard = {"Temperature": [98, 100], "Heart Rate": [60, 99], "Pulse": [60, 99], "BPSYS": [90, 120], "BPDIA": [60, 80],
+            "Oxygen Saturation": [0.95, 1], "PH": [7.3, 7.5], "Type": [0, 0]}
 data = data[features]
 
 reference_type = {0: "Normal", 1: "Mild", 2: "Severe", 3: "Chronic 1", 4: "Chronic 2", 5: "Chronic 3"}
@@ -23,8 +23,9 @@ class Node:
         self.data = None
         self.n = None
         self.data_for_each_type = [None, None, None, None, None, None]
-        self.H = None
-        self.IG = None
+        self.H = 0
+        self.IG = 0
+        self.branch = -1
         self.yes = None
         self.no = None
 
@@ -49,13 +50,30 @@ class Node:
         return self.H
 
     def calculate_IG(self):
-        self.IG = self.H - (self.yes.n*self.yes.H + self.no.n*self.no.H)/self.n
+        yes_H = 0
+        yes_n = 0
+        no_H = 0
+        no_n = 0
+        if self.yes is not None:
+            yes_H = self.yes.H
+            yes_n = self.yes.n
+        if self.no is not None:
+            no_H = self.no.H
+            no_n = self.no.n
+        self.IG = self.H - (yes_n * yes_H + no_n * no_H) / self.n
         return self.IG
 
     def print_node(self):
-        print(f"{features[self.level]} is normal?")
+        if self.branch == 0:
+            print("Branch no")
+        if self.branch == 1:
+            print("Branch yes")
+        print(f"{choose[self.level]} is normal?")
         print(f"Entropy: H(X) = {self.H}")
         print(f"IG = {self.IG}")
+        for i in range(0, len(self.data_for_each_type)):
+            print(f"{reference_type[i]} = {self.data_for_each_type[i][0]}")
+        print()
 
 
 def all_choices():
@@ -63,13 +81,13 @@ def all_choices():
     choice = []
 
     def recursive(i):
-        for feature in features:
+        for feature in features[:-1]:
             if feature not in choice:
                 choice.append(feature)
-                if len(choice) == len(features):
+                if len(choice) == len(features[:-1]):
                     res.append(choice.copy())
                 else:
-                    recursive(i+1)
+                    recursive(i + 1)
             else:
                 continue
             choice.pop()
@@ -79,36 +97,68 @@ def all_choices():
 
 
 def create_tree(plan):
-    queue = Queue(maxsize=10)
+    queue = Queue(maxsize=200)
     tree = Node(0)
     tree.data = data
     tree.process_elements()
+    tree.calculate_H()
     queue.put(tree)
     while not queue.empty():
         node = queue.get()
-        if node.level == len(plan)-1:
+        if node.level == len(plan) - 1:
             break
         node.yes = Node(node.level + 1)
         node.no = Node(node.level + 1)
         name = features[node.level]
         down = standard[name][0]
         up = standard[name][1]
-        node.yes.data = node.data[(node.data[name]>=down) & (node.data[name]<=up)]
-        node.no.data = node.data[(node.data[name]<down) | (node.data[name]>up)]
+        node.yes.data = node.data[(node.data[name] >= down) & (node.data[name] <= up)]
+        node.no.data = node.data[(node.data[name] < down) | (node.data[name] > up)]
         if len(node.yes.data) == 0:
             node.yes = None
         else:
             node.yes.process_elements()
+            node.yes.calculate_H()
+            node.yes.branch = 1
             queue.put(node.yes)
         if len(node.no.data) == 0:
             node.no = None
         else:
             node.no.process_elements()
+            node.no.calculate_H()
+            node.no.branch = 0
             queue.put(node.no)
 
     return tree
 
 
-# tree = create_tree(all_choices()[0][: 4])
-print(data.corr().to_string())
+def cal_IG_for_tree(tree):
+    q = Queue(maxsize=200)
+    q.put(tree)
+    while not q.empty():
+        node = q.get()
+        node.calculate_IG()
+        if node.yes is not None:
+            q.put(node.yes)
+        if node.no is not None:
+            q.put(node.no)
 
+
+def print_all_tree(tree):
+    q = Queue(maxsize=200)
+    q.put(tree)
+    while not q.empty():
+        node = q.get()
+        node.print_node()
+        if node.yes is not None:
+            q.put(node.yes)
+        if node.no is not None:
+            q.put(node.no)
+
+
+all = all_choices()
+choose = all[1000]
+print(choose)
+Tree = create_tree(choose)
+cal_IG_for_tree(Tree)
+print_all_tree(Tree)
