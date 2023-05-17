5e8a5044e1ad2a9b6dd7be0336fc22dd5382068e
Chu Mai <chuthimai18245@gmail.com>
2023-05-17 17:41:20 +0700

    Bai 08
    in ra H(X) phụ thuộc p q

--- FITSP23B21DCCN082DHW0108/FITSP23B21DCCN082CNDHW0108.py
+++ FITSP23B21DCCN082DHW0108/FITSP23B21DCCN082CNDHW0108.py
@@ -9,13 +9,13 @@ def Entropy(p, q, r):
     return -(p*math.log2(p) + q*math.log2(q) + r*math.log2(r))
 
 
-def draw_pqr():
+def draw_pqH():
     p = []
     q = []
     r = []
     H = []
 
-    for i in range(0, 1000):
+    for i in range(0, 3000):
         a = rd.random()
         p.append(a)
         b = 1
@@ -25,46 +25,42 @@ def draw_pqr():
         r.append(1 - a - b)
         entropy = Entropy(a, b, 1 - a - b)
         H.append(entropy)
-        # print(f"(p, q, r) = ({a}, {b}, {1-a-b}) => H = {entropy}")
 
-    fig = plt.figure("Đồ thị tập giá trị kết quả theo p, q, r")
+    fig1 = plt.figure("Đồ thị tập giá trị của H(X) theo p và q")
+    ax1 = plt.axes(projection="3d")
     p = np.array(p)
     q = np.array(q)
-    r = np.array(r)
-    ax = plt.axes(projection="3d")
-    ax.scatter3D(q, p, r, c=q, cmap=plt.get_cmap("cool"))
-    ax.set_xlabel("p")
-    ax.set_ylabel("q")
-    ax.set_zlabel("r")
+    H = np.array(H)
+    ax1.scatter3D(p, q, H, c=H, cmap=plt.get_cmap("gist_rainbow_r"))
+    ax1.set_xlabel("p")
+    ax1.set_ylabel("q")
+    ax1.set_zlabel("H(X)")
 
 
 def Hp0_max(p0):
     p = 1
-    q = 1
     H = []
-    # print(f"Với r = {p0} ta có (p, q, H):")
     for i in range(0, 100):
         while p > 1-p0:
             p = rd.random()
         q = 1-p0-p
         entropy = Entropy(p, q, p0)
         H.append(entropy)
-        # print(f"({p}, {q}, {entropy})", end=" ")
     return max(H)
 
 
 def draw_Hp0_max():
-    p0 = [rd.random() for i in range(0, 1000)]
+    p0 = [rd.random() for i in range(0, 3000)]
     fig = plt.figure("Đồ thị tập giá trị Hp0_max theo p0")
     Hp0 = np.array([Hp0_max(p) for p in p0])
     p0 = np.array(p0)
-    plt.scatter(p0, Hp0, c=p0, cmap="rainbow", marker=".")
+    plt.scatter(p0, Hp0, c=Hp0, cmap=plt.get_cmap("jet"), marker=".")
     plt.xlabel("p0")
     plt.ylabel("Hp0")
 
 
 draw_Hp0_max()
-draw_pqr()
+draw_pqH()
 
 plt.show()
 
